cd8804d1a4387e078652e662b9b9ffbfd046ffb7
hadioetomo <47894633+hadioetomo@users.noreply.github.com>
2026-09-11 22:15:31 +0700

    Update utils_wa.py

--- utils_wa.py
+++ utils_wa.py
@@ -1,6 +1,7 @@
 import requests
 
 WHATSAPP_API_URL = "https://api.watzap.id/v1/send_message"
+WHATSAPP_FILE_URL = "https://api.watzap.id/v1/send_file_url"
 WHATSAPP_API_KEY = "V3ELWOCBWBWHDEMX"
 WHATSAPP_NUMBER_KEY = "4Kpb4E1ohwAcU7XT"
 
@@ -13,6 +14,21 @@ def send_whatsapp_message(phone_number, message):
     }
     try:
         response = requests.post(WHATSAPP_API_URL, json=payload, timeout=15)
+        return response.json()
+    except Exception as e:
+        return {"status": False, "message": str(e)}
+
+def send_whatsapp_file(phone_number, file_url, caption):
+    payload = {
+        "api_key": WHATSAPP_API_KEY,
+        "number_key": WHATSAPP_NUMBER_KEY,
+        "phone_no": phone_number,
+        "url_file": file_url,
+        "filename": "Berita_Acara_Negosiasi.pdf",
+        "message": caption
+    }
+    try:
+        response = requests.post(WHATSAPP_FILE_URL, json=payload, timeout=20)
         return response.json()
     except Exception as e:
         return {"status": False, "message": str(e)}
